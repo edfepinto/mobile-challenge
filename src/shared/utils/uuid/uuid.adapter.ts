d5e8616bdc6f4b93c7b01fb97d8dbb7f309b635b
Eduardo Pinto <edfepinto@gmail.com>
2022-12-01 17:05:43 -0300

    feat: add uuid port and adapter using uuid lib

---
+++ src/shared/utils/uuid/uuid.adapter.ts
@@ -0,0 +1,11 @@
+import 'react-native-get-random-values';
+import { v4 } from "uuid";
+
+import { UuidPort } from "./uuid.port";
+
+export class UuidAdapter implements UuidPort {
+  generate(): string {
+    return v4();
+  }
+}
+
