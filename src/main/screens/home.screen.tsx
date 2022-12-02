178356fb99a37c3a1b39ff2e0b2c7d50228659f7
Eduardo Pinto <edfepinto@gmail.com>
2022-12-02 06:43:05 -0300

    feat: add a example of styled component

--- src/main/screens/home.screen.tsx
+++ src/main/screens/home.screen.tsx
@@ -1,11 +1,20 @@
 import * as React from "react";
 import { Text, View } from "react-native";
 import { t } from "~/shared/intl/translator";
+import styled from "styled-components/native";
+
+interface WordsProps {
+    color: string
+}
+
+const Words = styled.Text<WordsProps>`
+  color: ${(props) => props.color};
+`;
 
 export function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>{t("Home!")}</Text>
+      <Words color="red">{t("Home!")}</Words>
     </View>
   );
 }
