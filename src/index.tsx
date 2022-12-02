597f1ee3b6994b70178791a6188fda715a4969a6
Eduardo Pinto <edfepinto@gmail.com>
2022-12-02 01:26:06 -0300

    chore: move the react native entrypoint to point index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import { registerRootComponent } from "expo";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { UuidAdapter } from "~/shared/utils/uuid/uuid.adapter";
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Open up App.tsx to start working on your app!
+        <Text>{new UuidAdapter().generate()}</Text>
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default registerRootComponent(App);
