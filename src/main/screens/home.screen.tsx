eaaba2757a989106716c29b7895d0d38613a37c7
Eduardo Pinto <edfepinto@gmail.com>
2022-12-06 13:20:00 -0300

    feat: add shufle of event cards

--- src/main/screens/home.screen.tsx
+++ src/main/screens/home.screen.tsx
@@ -3,6 +3,7 @@ import { ScrollView } from "react-native";
 import { EventEntityBody } from "~/domain/entities/event.entity";
 import { Carousel } from "~/main/components/carousel.component";
 import eventsInMemoryRepository from "../repositories/events/events-in-memory.repository";
+import { shuffleArray } from "~/shared/utils/array/shuffle-array";
 
 export function HomeScreen() {
   const [events, setEvents] = React.useState<EventEntityBody[]>([]);
@@ -18,7 +19,7 @@ export function HomeScreen() {
     <ScrollView style={{ paddingHorizontal: 10, paddingVertical: 15 }}>
       {Array.from({ length: 10 }).map((_, index) => (
         <Carousel
-          data={events.filter((card) => card.category === "Shows e eventos")}
+          data={shuffleArray(events.filter((card) => card.category === "Shows e eventos"))}
           category={index % 2 == 0 ? "Shows e eventos" : "Passeios e tours"}
           key={index}
         />
---
+++ src/shared/utils/array/shuffle-array.ts
@@ -0,0 +1,16 @@
+// Ref: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+
+export function shuffleArray(array: Array<any>) {
+  let currentIndex = array.length,  randomIndex;
+
+  while (currentIndex != 0) {
+
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
